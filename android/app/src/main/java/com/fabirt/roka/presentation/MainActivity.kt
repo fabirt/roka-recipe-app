08e5d0cee1e5f9a724a03ba922ada587aa514450
Fabirt <fandig27@gmail.com>
2020-07-25 14:16:37 -0500

    Creating launcher icon and theme

--- android/app/src/main/java/com/fabirt/roka/presentation/MainActivity.kt
+++ android/app/src/main/java/com/fabirt/roka/presentation/MainActivity.kt
@@ -7,6 +7,7 @@ import com.fabirt.roka.R
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.AppTheme)
         setContentView(R.layout.activity_main)
     }
 }
