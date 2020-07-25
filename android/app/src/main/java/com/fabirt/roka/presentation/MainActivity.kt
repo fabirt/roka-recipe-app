928bb3a5c2489cb1e1695f841f209324e97451f4
Fabirt <fandig27@gmail.com>
2020-07-25 18:07:31 -0500

    Setup navigation component

--- android/app/src/main/java/com/fabirt/roka/presentation/MainActivity.kt
+++ android/app/src/main/java/com/fabirt/roka/presentation/MainActivity.kt
@@ -2,12 +2,23 @@ package com.fabirt.roka.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.fabirt.roka.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTheme(R.style.AppTheme)
         setContentView(R.layout.activity_main)
+        setupBottomNavigation()
+    }
+
+    private fun setupBottomNavigation() {
+        val navController = findNavController(R.id.mainNavHostFragment)
+        bottomNav.setupWithNavController(navController)
+        // Prevent recreate the current fragment
+        bottomNav.setOnNavigationItemReselectedListener {}
     }
 }
---
+++ android/app/src/main/java/com/fabirt/roka/presentation/favorites/FavoritesFragment.kt
@@ -0,0 +1,18 @@
+package com.fabirt.roka.presentation.favorites
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.fabirt.roka.R
+
+class FavoritesFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_favorites, container, false)
+    }
+}
