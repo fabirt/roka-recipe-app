aaf17b2ab1df217048885bbf9236bc6ba918362b
Fabirt <fandig27@gmail.com>
2020-07-25 13:05:48 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# roka-recipe-app
-Recipes and Food Plan App
+# Roka Recipes and Food App
+Personal project developed with the Android SDK.
+
+### Design
+Complete design at [creative market.](https://creativemarket.com/betush/3703359-Roka-Recipes-and-Food-Plan-App-UI)
---
+++ android/app/src/main/java/com/fabirt/roka/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.fabirt.roka.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.fabirt.roka.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
