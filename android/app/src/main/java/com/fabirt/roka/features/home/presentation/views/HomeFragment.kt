1511e9e609329f22cc4f538fd0294f13f10d0780
Fabirt <fandig27@gmail.com>
2020-07-26 09:15:34 -0500

    Modifying category layout

--- android/app/src/main/java/com/fabirt/roka/features/home/presentation/views/HomeFragment.kt
+++ android/app/src/main/java/com/fabirt/roka/features/home/presentation/views/HomeFragment.kt
@@ -1,6 +1,7 @@
 package com.fabirt.roka.features.home.presentation.views
 
 import android.os.Bundle
+import android.os.Parcelable
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -13,6 +14,7 @@ import com.fabirt.roka.features.home.presentation.adapters.CategoriesAdapter
 import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
