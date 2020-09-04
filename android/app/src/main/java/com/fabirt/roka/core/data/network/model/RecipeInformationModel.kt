89b7fba1c1e87099bfc5b4140f644e805508cefb
Fabirt <fandig27@gmail.com>
2020-09-04 14:06:09 -0500

    Creating fake data

--- android/app/src/main/java/com/fabirt/roka/core/data/network/model/RecipeInformationModel.kt
+++ android/app/src/main/java/com/fabirt/roka/core/data/network/model/RecipeInformationModel.kt
@@ -3,18 +3,229 @@ package com.fabirt.roka.core.data.network.model
 import com.google.gson.annotations.SerializedName
 
 data class RecipeInformationModel(
-    @SerializedName("id")
     val id: Int,
-    @SerializedName("sourceName")
     val sourceName: String?,
-    @SerializedName("title")
     val title: String,
-    @SerializedName("readyInMinutes")
     val readyInMinutes: Int?,
-    @SerializedName("servings")
     val servings: Int?,
-    @SerializedName("sourceUrl")
     val sourceUrl: String?,
     @SerializedName("image")
-    val imageUrl: String
+    val imageUrl: String,
+    val summary: String?,
+    @SerializedName("analyzedInstructions")
+    val instructions: List<RecipeInstructions>?
 )
+
+data class RecipeInstructions(
+    val steps: List<InstructionStep>
+)
+
+data class InstructionStep(
+    val number: Int,
+    val step: String,
+    val ingredients: List<RecipeElement>,
+    val equipment: List<RecipeElement>
+)
+
+data class RecipeElement(
+    val id: Int,
+    val name: String,
+    val image: String
+)
+
+/*
+    {
+        "vegetarian": false,
+        "vegan": false,
+        "glutenFree": false,
+        "dairyFree": false,
+        "veryHealthy": true,
+        "cheap": false,
+        "veryPopular": false,
+        "sustainable": false,
+        "weightWatcherSmartPoints": 11,
+        "gaps": "no",
+        "lowFodmap": false,
+        "aggregateLikes": 2,
+        "spoonacularScore": 91.0,
+        "healthScore": 90.0,
+        "creditsText": "Foodista.com – The Cooking Encyclopedia Everyone Can Edit",
+        "license": "CC BY 3.0",
+        "sourceName": "Foodista",
+        "pricePerServing": 168.12,
+        "id": 654959,
+        "title": "Pasta With Tuna",
+        "readyInMinutes": 45,
+        "servings": 4,
+        "sourceUrl": "http://www.foodista.com/recipe/K6QWSKQM/pasta-with-tuna",
+        "image": "https://spoonacular.com/recipeImages/654959-312x231.jpg",
+        "imageType": "jpg",
+        "summary": "Pasta With Tuna might be just the main course you are searching for. One serving contains <b>421 calories</b>, <b>24g of protein</b>, and <b>10g of fat</b>. For <b>$1.68 per serving</b>, this recipe <b>covers 28%</b> of your daily requirements of vitamins and minerals. 1 person were impressed by this recipe. Head to the store and pick up flour, onion, peas, and a few other things to make it today. It is a good option if you're following a <b>pescatarian</b> diet. All things considered, we decided this recipe <b>deserves a spoonacular score of 92%</b>. This score is excellent. Try <a href=\"https://spoonacular.com/recipes/pasta-and-tuna-salad-ensalada-de-pasta-y-atn-226303\">Pastan and Tuna Salad (Ensalada de Pasta y Atún)</a>, <a href=\"https://spoonacular.com/recipes/tuna-pasta-565100\">Tuna Pasta</a>, and <a href=\"https://spoonacular.com/recipes/tuna-pasta-89136\">Tuna Pasta</a> for similar recipes.",
+        "cuisines": [],
+        "dishTypes": [
+            "lunch",
+            "main course",
+            "main dish",
+            "dinner"
+        ],
+        "diets": [
+            "pescatarian"
+        ],
+        "occasions": [],
+        "analyzedInstructions": [
+            {
+                "name": "",
+                "steps": [
+                    {
+                        "number": 1,
+                        "step": "Cook pasta in a large pot of boiling water until al dente.",
+                        "ingredients": [
+                            {
+                                "id": 20420,
+                                "name": "pasta",
+                                "localizedName": "pasta",
+                                "image": "fusilli.jpg"
+                            },
+                            {
+                                "id": 14412,
+                                "name": "water",
+                                "localizedName": "water",
+                                "image": "water.png"
+                            }
+                        ],
+                        "equipment": [
+                            {
+                                "id": 404752,
+                                "name": "pot",
+                                "localizedName": "pot",
+                                "image": "stock-pot.jpg"
+                            }
+                        ]
+                    },
+                    {
+                        "number": 2,
+                        "step": "Drain and return to warm pot. Put olive oil in saucepan and add onion.",
+                        "ingredients": [
+                            {
+                                "id": 4053,
+                                "name": "olive oil",
+                                "localizedName": "olive oil",
+                                "image": "olive-oil.jpg"
+                            },
+                            {
+                                "id": 11282,
+                                "name": "onion",
+                                "localizedName": "onion",
+                                "image": "brown-onion.png"
+                            }
+                        ],
+                        "equipment": [
+                            {
+                                "id": 404669,
+                                "name": "sauce pan",
+                                "localizedName": "sauce pan",
+                                "image": "sauce-pan.jpg"
+                            },
+                            {
+                                "id": 404752,
+                                "name": "pot",
+                                "localizedName": "pot",
+                                "image": "stock-pot.jpg"
+                            }
+                        ]
+                    },
+                    {
+                        "number": 3,
+                        "step": "Saute until transparent. Stir in flour and cook for a few seconds and then whisk in milk. Stir constantly until this thickens.",
+                        "ingredients": [
+                            {
+                                "id": 20081,
+                                "name": "all purpose flour",
+                                "localizedName": "all purpose flour",
+                                "image": "flour.png"
+                            },
+                            {
+                                "id": 1077,
+                                "name": "milk",
+                                "localizedName": "milk",
+                                "image": "milk.png"
+                            }
+                        ],
+                        "equipment": [
+                            {
+                                "id": 404661,
+                                "name": "whisk",
+                                "localizedName": "whisk",
+                                "image": "whisk.png"
+                            }
+                        ]
+                    },
+                    {
+                        "number": 4,
+                        "step": "Add peas, tuna (shredded into chunks,) parsley, green onions, cheese and hot pepper sauce.",
+                        "ingredients": [
+                            {
+                                "id": 6168,
+                                "name": "hot sauce",
+                                "localizedName": "hot sauce",
+                                "image": "hot-sauce-or-tabasco.png"
+                            },
+                            {
+                                "id": 11291,
+                                "name": "green onions",
+                                "localizedName": "green onions",
+                                "image": "spring-onions.jpg"
+                            },
+                            {
+                                "id": 11297,
+                                "name": "parsley",
+                                "localizedName": "parsley",
+                                "image": "parsley.jpg"
+                            },
+                            {
+                                "id": 1041009,
+                                "name": "cheese",
+                                "localizedName": "cheese",
+                                "image": "cheddar-cheese.png"
+                            },
+                            {
+                                "id": 11304,
+                                "name": "peas",
+                                "localizedName": "peas",
+                                "image": "peas.jpg"
+                            },
+                            {
+                                "id": 10015121,
+                                "name": "tuna",
+                                "localizedName": "tuna",
+                                "image": "canned-tuna.png"
+                            }
+                        ],
+                        "equipment": []
+                    },
+                    {
+                        "number": 5,
+                        "step": "Pour over pasta and stir gently to mix.",
+                        "ingredients": [
+                            {
+                                "id": 20420,
+                                "name": "pasta",
+                                "localizedName": "pasta",
+                                "image": "fusilli.jpg"
+                            }
+                        ],
+                        "equipment": []
+                    },
+                    {
+                        "number": 6,
+                        "step": "Serve at once.",
+                        "ingredients": [],
+                        "equipment": []
+                    }
+                ]
+            }
+        ],
+        "spoonacularSourceUrl": "https://spoonacular.com/pasta-with-tuna-654959"
+    }
+
+*/
--- android/app/src/main/java/com/fabirt/roka/features/search/presentation/view_model/SearchViewModel.kt
+++ android/app/src/main/java/com/fabirt/roka/features/search/presentation/view_model/SearchViewModel.kt
@@ -6,7 +6,10 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.fabirt.roka.core.data.network.client.RecipesApiClient
+import com.fabirt.roka.core.data.network.model.InstructionStep
+import com.fabirt.roka.core.data.network.model.RecipeElement
 import com.fabirt.roka.core.data.network.model.RecipeInformationModel
+import com.fabirt.roka.core.data.network.model.RecipeInstructions
 import kotlinx.coroutines.launch
 
 class SearchViewModel : ViewModel() {
@@ -20,11 +23,160 @@ class SearchViewModel : ViewModel() {
         if (_recipes.value?.isNotEmpty() == true) return
         viewModelScope.launch {
             try {
-                val result = service.searchRecipes("pasta", true)
-                _recipes.value = result.results
+                //val result = service.searchRecipes("pasta", true)
+                _recipes.value = getFakeData()
             } catch (e: Exception) {
                 Log.e("SearchViewModel", e.toString())
             }
         }
+    }
+
+    private fun getFakeData(): List<RecipeInformationModel> {
+        val data = RecipeInformationModel(
+            1,
+            "Foodista",
+            "Pasta With Tuna",
+            45,
+            4,
+            "",
+            "https://spoonacular.com/recipeImages/654959-312x231.jpg",
+            "Summary",
+            listOf(
+                RecipeInstructions(
+                    listOf(
+                        InstructionStep(
+                            1,
+                            "Cook pasta in a large pot of boiling water until al dente.",
+                            listOf(
+                                RecipeElement(
+                                    20420,
+                                    "pasta",
+                                    ""
+                                ),
+                                RecipeElement(
+                                    14412,
+                                    "water",
+                                    ""
+                                )
+                            ),
+                            listOf(
+                                RecipeElement(
+                                    404752,
+                                    "pot",
+                                    ""
+                                )
+                            )
+                        ),
+                        InstructionStep(
+                            2,
+                            "Drain and return to warm pot. Put olive oil in saucepan and add onion.",
+                            listOf(
+                                RecipeElement(
+                                    4053,
+                                    "olive oil",
+                                    ""
+                                ),
+                                RecipeElement(
+                                    11282,
+                                    "onion",
+                                    ""
+                                )
+                            ),
+                            listOf(
+                                RecipeElement(
+                                    404669,
+                                    "sauce pan",
+                                    ""
+                                ),
+                                RecipeElement(
+                                    404752,
+                                    "pot",
+                                    ""
+                                )
+                            )
+                        ),
+                        InstructionStep(
+                            3,
+                            "Saute until transparent. Stir in flour and cook for a few seconds and then whisk in milk. Stir constantly until this thickens.",
+                            listOf(
+                                RecipeElement(
+                                    20081,
+                                    "all purpose flour",
+                                    ""
+                                ),
+                                RecipeElement(
+                                    1077,
+                                    "milk",
+                                    ""
+                                )
+                            ),
+                            listOf(
+                                RecipeElement(
+                                    404661,
+                                    "whisk",
+                                    ""
+                                )
+                            )
+                        ),
+                        InstructionStep(
+                            4,
+                            "Add peas, tuna (shredded into chunks,) parsley, green onions, cheese and hot pepper sauce.",
+                            listOf(
+                                RecipeElement(
+                                    6168,
+                                    "hot sauce",
+                                    ""
+                                ),
+                                RecipeElement(
+                                    11291,
+                                    "green onions",
+                                    ""
+                                ),
+                                RecipeElement(
+                                    11297,
+                                    "parsley",
+                                    ""
+                                ),
+                                RecipeElement(
+                                    1041009,
+                                    "cheese",
+                                    ""
+                                ),
+                                RecipeElement(
+                                    11304,
+                                    "peas",
+                                    ""
+                                ),
+                                RecipeElement(
+                                    10015121,
+                                    "tuna",
+                                    ""
+                                )
+                            ),
+                            listOf()
+                        ),
+                        InstructionStep(
+                            5,
+                            "Pour over pasta and stir gently to mix.",
+                            listOf(
+                                RecipeElement(
+                                    20420,
+                                    "pasta",
+                                    ""
+                                )
+                            ),
+                            listOf()
+                        ),
+                        InstructionStep(
+                            6,
+                            "Serve at once.",
+                            listOf(),
+                            listOf()
+                        )
+                    )
+                )
+            )
+        )
+        return listOf(data)
     }
 }
