6197f9e356341293b49be1fc1b22171077a47567
Fabirt <fandig27@gmail.com>
2020-09-06 08:44:25 -0500

    Creating models

---
+++ android/app/src/main/java/com/fabirt/roka/features/detail/domain/model/RecipeDetailItem.kt
@@ -0,0 +1,21 @@
+package com.fabirt.roka.features.detail.domain.model
+
+import com.fabirt.roka.core.data.network.model.RecipeElement
+
+sealed class RecipeDetailItem {
+    abstract val id: Int
+
+    data class Summary(val text: String, override val id: Int) : RecipeDetailItem()
+
+    data class SectionTitle(val text: String, override val id: Int) : RecipeDetailItem()
+
+    data class RecipeIngredient(val element: RecipeElement) : RecipeDetailItem() {
+        override val id: Int
+            get() = element.id
+    }
+
+    data class RecipeDirection(val element: RecipeElement) : RecipeDetailItem() {
+        override val id: Int
+            get() = element.id
+    }
+}
