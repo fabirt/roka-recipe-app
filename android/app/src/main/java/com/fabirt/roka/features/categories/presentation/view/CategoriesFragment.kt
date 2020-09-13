0d9a24d6ef1ae6751406e89d7a06c687edc2841e
Fabirt <fandig27@gmail.com>
2020-09-12 21:19:46 -0500

    Creating blank fragment for category detail

--- android/app/src/main/java/com/fabirt/roka/features/categories/presentation/view/CategoriesFragment.kt
+++ android/app/src/main/java/com/fabirt/roka/features/categories/presentation/view/CategoriesFragment.kt
@@ -5,11 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.fabirt.roka.R
+import com.fabirt.roka.features.categories.domain.model.CategoryItem
 import com.fabirt.roka.features.categories.presentation.adapters.CategoriesAdapter
 import com.fabirt.roka.features.categories.presentation.viewmodel.CategoriesViewModel
 import kotlinx.android.synthetic.main.fragment_categories.*
@@ -29,9 +30,7 @@ class CategoriesFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        adapter = CategoriesAdapter() { item ->
-            Toast.makeText(requireContext(), item.name, Toast.LENGTH_SHORT).show()
-        }
+        adapter = CategoriesAdapter(::navigateToCategoryDetail)
         rvCategories.layoutManager = LinearLayoutManager(requireContext())
         rvCategories.adapter = adapter
 
@@ -40,5 +39,10 @@ class CategoriesFragment : Fragment() {
             rvCategories.scheduleLayoutAnimation()
             adapter.submitList(categories)
         })
+    }
+
+    private fun navigateToCategoryDetail(category: CategoryItem) {
+        val action = CategoriesFragmentDirections.actionCategoriesFragmentToCategoryDetailFragment()
+        findNavController().navigate(action)
     }
 }
---
+++ android/app/src/main/java/com/fabirt/roka/features/categories/presentation/view/CategoryDetailFragment.kt
@@ -0,0 +1,18 @@
+package com.fabirt.roka.features.categories.presentation.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.fabirt.roka.R
+
+class CategoryDetailFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_category_detail, container, false)
+    }
+}
