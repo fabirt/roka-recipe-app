95f4888d0486d9a4b964c73588d2eafabe16ec32
Fabirt <fandig27@gmail.com>
2020-09-13 14:14:02 -0500

    Reading from DataStore. Dynamic start destination.

--- android/app/src/main/java/com/fabirt/roka/features/MainActivity.kt
+++ android/app/src/main/java/com/fabirt/roka/features/MainActivity.kt
@@ -4,8 +4,16 @@ import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.datastore.preferences.createDataStore
+import androidx.datastore.preferences.preferencesKey
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.Navigation
 import com.fabirt.roka.R
+import com.fabirt.roka.core.constants.K
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -14,6 +22,7 @@ class MainActivity : AppCompatActivity() {
         setTheme(R.style.AppTheme)
         setContentView(R.layout.activity_main)
         configureStatusBarForFullscreen()
+        setupNavigation()
     }
 
     private fun configureStatusBarForFullscreen() {
@@ -28,4 +37,22 @@ class MainActivity : AppCompatActivity() {
         window.decorView.systemUiVisibility = flags
     }
 
+    private fun setupNavigation() {
+        val navController = Navigation.findNavController(this, R.id.mainNavHostFragment)
+        val navGraph = navController.navInflater.inflate(R.navigation.main_graph)
+        lifecycleScope.launch {
+            val dataStore = createDataStore(name = K.SETTINGS_DATA_STORE_NAME)
+            val prefKey = preferencesKey<Boolean>(K.ONBOARDING_DID_SHOW_KEY)
+            val onBoardingDidShowFlow = dataStore.data.map { preferences ->
+                preferences[prefKey] ?: false
+            }
+            val didShow = onBoardingDidShowFlow.first()
+            var startDestination = R.id.onboardingFragment
+            if (didShow) {
+                startDestination = R.id.homeFragment
+            }
+            navGraph.startDestination = startDestination
+            navController.graph = navGraph
+        }
+    }
 }
